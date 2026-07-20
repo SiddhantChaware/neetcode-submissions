53462b075cd44787d319f968e99dab0abfbec07f
Siddhant Chaware <147546941+SiddhantChaware@users.noreply.github.com>
2026-07-20 11:45:32 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solve(int idx,int n,int[] dp){
+        if(idx > n){
+            return 0;
+        }
+        if(idx == n){
+            return 1;
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
+
+        int take = solve(idx+1,n,dp);
+        int skip = solve(idx+2,n,dp);
+
+        return dp[idx] = take + skip;
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return solve(0,n,dp);
+    }
+}
